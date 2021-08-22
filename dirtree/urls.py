12bada3869aa294206407cedac0aaf5c2f45312a
LOKESWARAN ARULJOTHI <11lokeswaran@gmail.com>
2021-08-22 16:09:00 +0530

    Class based view is created and code is cleaned

--- dirtree/urls.py
+++ dirtree/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Home
 
+# all urls in the app
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', Home.as_view(), name="home"),
     path('number-of-trees-generated', views.no_of_trees)
 ]
--- dirtree/views.py
+++ dirtree/views.py
@@ -1,58 +1,64 @@
+import os
+import json
+import requests as r
+from dotenv import load_dotenv
+from django.views import View
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests as r
-import os
-from dotenv import load_dotenv
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
-import json
 
-# Create your views here.
 no_of_trees_generated = 0
 load_dotenv()
-@ensure_csrf_cookie
-def index(request):
-    if request.method == 'POST':
+
+# Class based view for Home page
+class Home(View):
+    # Get method to render the home page
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request):
+        return render(request, "dirtree/index.html")
+    
+    # Post method that gets the url and generates Tree
+    @method_decorator(ensure_csrf_cookie)
+    def post(self, request):
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
         repourl = body['repourl']
-        link_in_parts = list(map(str, repourl.strip().split('/')))
+        link_in_parts = list(map(str, repourl.strip().split('/')))  #Splits the urls to get the username and repo name
         github_index = None
-        for i in range(len(link_in_parts)):
+        for i in range(len(link_in_parts)):    # Finds the index of github.com to give the index of username and repo name
             if link_in_parts[i] == "github.com":
                 github_index = i
                 break
-        user_name = link_in_parts[github_index+1]
-        repository_name = link_in_parts[github_index+2]
+        user_name = link_in_parts[github_index+1]    # Github Username is stored in user_name
+        repository_name = link_in_parts[github_index+2]    # Github Repository name is stored in repository_name
         files_list = get_files([], user_name, repository_name, '')
         routes = create_Node(files_list)
-        request.session['routes'] = routes
         global no_of_trees_generated
         no_of_trees_generated += 1
         return HttpResponse(json.dumps(routes), content_type="application/json")
-    elif request.method == "GET":
-        print(request.method)
-        routes = request.session.get('routes',False)
-        if routes: del request.session['routes']
-        return render(request, "dirtree/index.html", context={"routes": routes})
-    
+
+# number-of-trees-generated urlpattern response function  
 def no_of_trees(request):
     return HttpResponse(no_of_trees_generated)
 
+# Uses Github api to get the files and directories paths
 def get_files(paths, user_name, repository_name, path=''):
-    response = r.get("https://api.github.com/repos/"+user_name+"/"+repository_name+"/commits", auth=(os.environ.get('GITHUB_USERNAME'), os.environ.get('TOKEN_KEY'))).json()
-    trees = r.get(response[0]["commit"]["tree"]["url"]+"?recursive=true").json()["tree"]
+    response = r.get("https://api.github.com/repos/"+user_name+"/"+repository_name+"/commits", auth=(os.environ.get('GITHUB_USERNAME'), os.environ.get('TOKEN_KEY'))).json()    # Returns all the commits made in the repository
+    trees = r.get(response[0]["commit"]["tree"]["url"]+"?recursive=true").json()["tree"]    # gets the paths of files and directories from the latest commit
     paths=[]
     for tree in trees:
         paths.append(tree["path"])
     return paths
 
-
+# Tree class to generate tree for a repo
 class Node():
     def __init__(self,data):
         self.data = data
         self.children = []
         self.parent = None
     
+    # returns the level of the node
     def get_level(self):
         level = 0
         p = self.parent
@@ -61,21 +67,17 @@ class Node():
             p = p.parent
         return level
 
+    # Adds the child node to the parent node
     def add_child(self, child):
-        if self.children != []:
-            for c in self.children:
-                # print(c.data)
-                if child.data == c.data:
-                    return c           
-            else:
-                child.parent = self
-                self.children.append(child)
-                return self.children[-1]
+        for c in self.children:
+            if child.data == c.data:
+                return c    # if child already exists, returns it         
         else:
             child.parent = self
             self.children.append(child)
-            return self.children[-1]
+            return self.children[-1]    # appends the new child
         
+    # dynamically returns the prefix of the files or directory in tree nodes
     def get_prefix(self, prefix):
         if self.parent:
             if self.parent.parent:
@@ -92,36 +94,37 @@ class Node():
             prefix = '' + prefix
         return prefix
     
-    def print_Node(self,last_child = True, tree=[]):
+    # returns the Tree of the repository
+    def get_tree(self,last_child = True, tree=[]):
         if self.data == "root":
             tree.append('.')
         else:
             prefix = ''
             prefix = self.get_prefix('')
-            if last_child:
+            if last_child:    # adds prefix to the files or directory in tree nodes
                 full_sentence = prefix + "└── " + self.data
             else:
                 full_sentence = prefix + "├── " + self.data
             tree.append(full_sentence)
             
-        # last_parent = last_child
-        if self.children:
+        if self.children:    # recursively calls the get_tree() if it has child
             for child in self.children:
                 if child != self.children[-1]:
                     last_child = False
                 else:
                     last_child = True
-                child.print_Node(last_child,tree)
+                child.get_tree(last_child,tree)
         
         return tree
 
+# Create tree with the paths and returns the tree
 def create_Node(files_list):
-    root1 = root = Node('root')
+    root_copy = root = Node('root')
     
-    for file_paths in files_list:
+    for file_paths in files_list:    # Loops through each path in the paths list
         file_path = file_paths.split('/')
-        for file in file_path:
+        for file in file_path:    # Loops throught each level from root to the file to add the child
             root = root.add_child(Node(file))
-        root = root1
+        root = root_copy
     
-    return root.print_Node(True,[])
+    return root.get_tree(True,[])
