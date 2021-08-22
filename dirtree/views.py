d4632fe752b91d25d8277f45dda2e5f47d8d28e0
LOKESWARAN ARULJOTHI <11lokeswaran@gmail.com>
2021-08-22 14:30:22 +0530

    no of tree generated func is added

--- dirtree/views.py
+++ dirtree/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.http import HttpResponse
 import requests as r
 import os
 from dotenv import load_dotenv
-from random import shuffle
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 
-load_dotenv()
 # Create your views here.
+no_of_trees_generated = 0
+load_dotenv()
 @ensure_csrf_cookie
 def index(request):
     if request.method == 'POST':
         body_unicode = request.body.decode('utf-8')
-        # print(body_unicode)
         body = json.loads(body_unicode)
         repourl = body['repourl']
         link_in_parts = list(map(str, repourl.strip().split('/')))
@@ -26,26 +24,26 @@ def index(request):
         user_name = link_in_parts[github_index+1]
         repository_name = link_in_parts[github_index+2]
         files_list = get_files([], user_name, repository_name, '')
-        # shuffle(files_list)
-        # print(files_list)
         routes = create_Node(files_list)
         request.session['routes'] = routes
+        global no_of_trees_generated
+        no_of_trees_generated += 1
         return HttpResponse(json.dumps(routes), content_type="application/json")
     elif request.method == "GET":
         print(request.method)
         routes = request.session.get('routes',False)
         if routes: del request.session['routes']
         return render(request, "dirtree/index.html", context={"routes": routes})
-
+    
+def no_of_trees(request):
+    return HttpResponse(no_of_trees_generated)
 
 def get_files(paths, user_name, repository_name, path=''):
     response = r.get("https://api.github.com/repos/"+user_name+"/"+repository_name+"/commits", auth=(os.environ.get('GITHUB_USERNAME'), os.environ.get('TOKEN_KEY'))).json()
     trees = r.get(response[0]["commit"]["tree"]["url"]+"?recursive=true").json()["tree"]
     paths=[]
     for tree in trees:
-        # paths.append((tree["path"],"dir" if tree["mode"]=="040000" else "file"))
         paths.append(tree["path"])
-    # print(paths)
     return paths
 
 
@@ -118,14 +116,11 @@ class Node():
         return tree
 
 def create_Node(files_list):
-    # print(files_list)
     root1 = root = Node('root')
     
     for file_paths in files_list:
         file_path = file_paths.split('/')
-        # print(file_path)
         for file in file_path:
-            # print(file)
             root = root.add_child(Node(file))
         root = root1
     
